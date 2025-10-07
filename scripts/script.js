98e685b592eeeee282f9efa9d1f73b1ec279ac3a
Arttu <arl00023@laurea.fi>
2025-10-07 15:34:58 +0300

    Testing solutions for localstorage

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,16 @@
 const inputField = document.getElementById('newEntry');
 const ul = document.querySelector('#taskList-cont');
 
+
+const createLi = () => {
+
+}
 //Adding new task to the list
 let addNew = () => {
         let taskText = inputField.value.trim()
-        if (taskText.length < 3) {
+        if (taskText.length < 3 && taskText.length > 100) {
             inputField.style.borderColor = 'red';
-            alert('Entries must be at least three characters');
+            alert('Entries must be between 3 and 100 characters');
         } else {
         let idIncrement = 1;
         let id = `item-${idIncrement++}`; // id format with incrementing numbers
@@ -25,19 +29,42 @@ let addNew = () => {
         taskListLabel.textContent = taskText;
         
 
-        // Line break after checkbox
+        // Create line break after checkbox
         let taskListBr = document.createElement('br');
 
-        // Assign new unique id for each list item
+        // Append checkbox elements to the li element
         taskLi.appendChild(taskListItem);
         taskLi.appendChild(taskListLabel);
         taskLi.appendChild(taskListBr);
 
-        ul.appendChild(taskLi)
+        ul.appendChild(taskLi) // Append li element with the checkbox elements
 
         inputField.value = ""; // Reset input field after list item addition
-        inputField.style.border = 'none';
+        inputField.style.border = 'none'; // Reset input field appeareance after prior invalid input
         }
 };
 
+
+
+const saveList = () => {
+    let storedValues = []; // Store list items to array
+    let liValues = document.querySelectorAll('.listItem');
+
+    for (let i = 0; i < liValues.length; i++) {
+        storedValues.push(liValues[i].innerHTML);
+    }
+
+    localStorage.setItem('liItems', JSON.stringify(storedValues));
+
+}
+
+const loadSaved = () => {
+    const savedValues = JSON.parse(localStorage.getItem('storedValues'))
+    console.log(savedValues)
+    //ul.appendChild(savedValues);
+}
+
 document.querySelector("#addBtn").addEventListener("click",addNew);
+document.querySelector("#addBtn").addEventListener("click",saveList);
+
+document.querySelector("#retrieveBtn").addEventListener("click",loadSaved);
