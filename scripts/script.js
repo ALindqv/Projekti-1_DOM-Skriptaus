284bfffa8ae4dbf05ca0d9413cfe2a6b6fc32348
Arttu <arl00023@laurea.fi>
2025-10-10 19:55:49 +0300

    Stable before changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,21 @@
 const inputField = document.querySelector('#newEntry');
 const ul = document.querySelector('#taskList-cont');
 let idIncrement = 1;
+let localStorageId = 1;
 
+//Load localstorage items
+let tasks = localStorage.getItem('tasks') ? 
+JSON.parse(localStorage.getItem('tasks')) : []; //Empty array if localstorage is empty
+
+tasks.forEach(itemAdd);
 
 const incrementId = () => {
-    var id = `item-${idIncrement++}`; // id format with incrementing numbers
+    let id = `task${idIncrement++}`; // Unique id for li elements
     return id;
 }
 
 // Create new checkbox li item
-const createListItem = () => {
+const createCheckBox = () => {
     // Create checkbox item
     let checkboxElem = document.createElement('input');
     checkboxElem.type = 'checkbox'; // Set list item as checkbox
@@ -17,79 +23,46 @@ const createListItem = () => {
 
     // Create label for checkbox
     let checkboxLabel = document.createElement('label');
-    checkboxLabel.className = 'cbLabel';
     checkboxLabel.htmlFor = id;
     
     return {checkboxElem, checkboxLabel};
 }
 
 // Adding new task to the list
-let itemAdd = () => {
+let itemAdd = (taskText) => {
     let taskText = inputField.value.trim() // Store user input to a variable
     if (taskText.length < 3) {
-        //inputField.style.borderColor = 'red';
         alert('Entry must be at least three characters');
+        return;
     } else {
-    id = incrementId(); // Get next id number
-    let {checkboxElem, checkboxLabel} = createListItem() // Get created checkbox and label
-    checkboxLabel.textContent = taskText; // Set user input value as the label
+        id = incrementId(); // Get next id number
+        let {checkboxElem, checkboxLabel} = createCheckBox() // Get created checkbox and label
+        checkboxLabel.textContent = taskText; // Set user input value as the label
 
-    let taskLi = document.createElement('li'); // Create li element to store checkbox and label
-    checkboxElem.id = id; // Set input element id
-    taskLi.className = 'listItem' // Li element main class name
-    taskLi.id = id; // Set li element id
+        let taskLi = document.createElement('li'); // Create li element to store checkbox and label
+        taskLi.className = 'listItem' // Li element main class name
+        checkboxElem.id = id; // Set input element id
+        
 
-    taskLi.append(checkboxElem, checkboxLabel); // Append checkbox and label to the li element as child elements
+        taskLi.append(checkboxElem, checkboxLabel); // Append checkbox and label to the li element as child elements
 
-    ul.appendChild(taskLi) // Append li element to target ul element with the checkbox elements
-
-    inputField.value = ""; // Reset input field after list item addition
-    //inputField.style.border = 'none'; // Reset input field appeareance after prior invalid input
+        ul.append(taskLi) // Append li element to target ul element with the checkbox elements
+        
+        inputField.value = ""; // Reset input field after list item addition
     }
 };
 
-// Removing task list elements
-const itemDel = () => {
-    var selected = document.querySelectorAll('.listItem-checkbox:checked'); // Find checked checkbox items
-    //Delete checked elements' parent li element
-    selected.forEach((elem) => {
-        elem.parentElement.remove();
-    })
-}
 
 
+// Save to localstorage
 const saveToStorage = () => {
-    // li element attributes
-    let liElem = document.querySelectorAll('.listItem');
-    let liClass = liElem.getAttribute('class');
-    let liId = liElem.getAttribute('id')
-
-    // Checkbox element + label attributes
-    let cbElem = document.querySelectorAll('.listItem-checkbox');
-    let cbLabel = document.querySelectorAll('.cbLabel');
-    let cbType = cbElem.getAttribute('type');
-    let cbClass = cbElem.getAttribute('class');
-    let cbId = cbElem.getAttribute('id')
-
-    /*
-    const liElem={
-        liClass: 'listItem',
-        liId: id
+    let cbObject = { 
+        type: 'checkbox',
+        class: 'listItem-checkbox',
+        id: id
     }
 
-    const inputElem={
-        inputType: 'checkbox',
-        inputClass: 'listItem-checkbox',
-        inputId: id
-    }
-
-    const liElemJSON = JSON.stringify(liElem)
-    const inputElemJSON = JSON.stringify(inputElem)
-
-    localStorage.setItem('liElem',liElemJSON);
-    localStorage.setItem('inputElem',inputElemJSON);
-    */
-
+    localStorage.setItem(id, JSON.stringify(cbObject));
 }
 
 const elementFromStorage = () => {
@@ -98,17 +71,40 @@ const elementFromStorage = () => {
     ul.appendChild(savedValues);
 }
 
+// Removing tasks from the list
+
+// Removing selected task list elements
+const delSelected = () => {
+    let selected = document.querySelectorAll('.listItem-checkbox:checked'); // Find checked checkbox items
+    let keyIndex = localStorage.key()
+    //Delete checked elements' parent li element
+    selected.forEach((elem) => {
+        elem.parentElement.remove();
+        localStorage.removeItem(keyIndex)
+    })
+}
+
+// Clear the entire list
+const clearList = () => {
+    let liItems = document.querySelectorAll('.listItem');
+    liItems.forEach((elem) => {
+        elem.remove();
+        localStorage.clear()
+    })
+}
+
+
 
 // Listeners
 document.querySelector("#addBtn").addEventListener("click",itemAdd); // Addition
-document.querySelector("#delBtn").addEventListener("click",itemDel); // Deletion
-document.querySelector("#storageSaveBtn").addEventListener("click",saveToStorage);
-//document.querySelector("#delBtn").addEventListener("click",loadSaved);
+document.querySelector("#addBtn").addEventListener("click",saveToStorage);
+document.querySelector("#delBtn").addEventListener("click",delSelected); // Deletion
+document.querySelector("#clearBtn").addEventListener("click",clearList);
 
 // Event listener for adding items using enter key
 inputField.addEventListener('keypress',function(e) {
     if (e.key === "Enter") {
-        e.preventDefault();
+        e.preventDefault(); // Disable key default functionality
         document.querySelector('#addBtn').click();
     }
 });
