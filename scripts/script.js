0a7730f997d86594e8b501a5b5bef95f23b7d745
Arttu <arl00023@laurea.fi>
2025-10-13 05:08:00 +0300

    CHeckpoint commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,110 +1,121 @@
+const STORAGE_KEY = 'task'
+
+// Static attributes for input
+const cbInputType = 'checkbox';
+const cbClass = 'listItem-checkbox';
+const liClass = 'listItem';
+
+// DOM Elements
 const inputField = document.querySelector('#newEntry');
 const ul = document.querySelector('#taskList-cont');
+const form = document.querySelector('#taskSubmit')
 let idIncrement = 1;
-let localStorageId = 1;
-
-//Load localstorage items
-let tasks = localStorage.getItem('tasks') ? 
-JSON.parse(localStorage.getItem('tasks')) : []; //Empty array if localstorage is empty
-
-tasks.forEach(itemAdd);
 
 const incrementId = () => {
     let id = `task${idIncrement++}`; // Unique id for li elements
     return id;
 }
 
-// Create new checkbox li item
-const createCheckBox = () => {
-    // Create checkbox item
-    let checkboxElem = document.createElement('input');
-    checkboxElem.type = 'checkbox'; // Set list item as checkbox
-    checkboxElem.className = 'listItem-checkbox'; // Different checkbox class name for easier styling
-
-    // Create label for checkbox
-    let checkboxLabel = document.createElement('label');
-    checkboxLabel.htmlFor = id;
-    
-    return {checkboxElem, checkboxLabel};
+//Load localstorage items
+const loadFromStorage = () => {
+    let taskArray = localStorage.getItem(STORAGE_KEY); 
+    return taskArray ? JSON.parse(taskArray) : []; //Empty array if localstorage is empty
 }
-
-// Adding new task to the list
-let itemAdd = (taskText) => {
-    let taskText = inputField.value.trim() // Store user input to a variable
-    if (taskText.length < 3) {
-        alert('Entry must be at least three characters');
-        return;
-    } else {
-        id = incrementId(); // Get next id number
-        let {checkboxElem, checkboxLabel} = createCheckBox() // Get created checkbox and label
-        checkboxLabel.textContent = taskText; // Set user input value as the label
-
-        let taskLi = document.createElement('li'); // Create li element to store checkbox and label
-        taskLi.className = 'listItem' // Li element main class name
-        checkboxElem.id = id; // Set input element id
-        
-
-        taskLi.append(checkboxElem, checkboxLabel); // Append checkbox and label to the li element as child elements
-
-        ul.append(taskLi) // Append li element to target ul element with the checkbox elements
-        
-        inputField.value = ""; // Reset input field after list item addition
-    }
-};
-
-
 
 // Save to localstorage
-const saveToStorage = () => {
-    let cbObject = { 
-        type: 'checkbox',
-        class: 'listItem-checkbox',
-        id: id
+const saveToStorage = (taskData) => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(taskData));
+}
+
+
+
+let createTask = () => {
+    // Create checkbox element
+    let checkboxElem = document.createElement('input');
+    checkboxElem.type = cbInputType; // Set list item as checkbox
+    checkboxElem.className = cbClass; // Different checkbox class name for easier styling
+    checkboxElem.id = incrementId(); // Set input element id
+
+    // Create label for checkbox element
+    let checkboxLabel = document.createElement('label');
+    checkboxLabel.textContent = inputField.value;
+    checkboxLabel.htmlFor = checkboxElem.id;
+    
+    const taskLi = document.createElement('li'); // Create li element to store checkbox and label
+    taskLi.className = liClass // Task element main class name
+    
+    taskLi.append(checkboxElem, checkboxLabel); // Append checkbox and label to the li element as child elements
+
+    ul.appendChild(task) // Append li element to target ul element with the checkbox elements
+}
+
+const renderFromStorage = () => {
+    ul.innerHTML = '';
+    const taskData = loadFromStorage();
+    for (let i = 0; i < taskData.length; i++) {
+        createTask();
     }
-
-    localStorage.setItem(id, JSON.stringify(cbObject));
 }
 
-const elementFromStorage = () => {
-    const savedValues = JSON.parse(localStorage.getItem('storedValues'))
-    console.log(savedValues)
-    ul.appendChild(savedValues);
-}
+
+
 
 // Removing tasks from the list
 
 // Removing selected task list elements
 const delSelected = () => {
     let selected = document.querySelectorAll('.listItem-checkbox:checked'); // Find checked checkbox items
-    let keyIndex = localStorage.key()
     //Delete checked elements' parent li element
     selected.forEach((elem) => {
         elem.parentElement.remove();
-        localStorage.removeItem(keyIndex)
     })
 }
 
 // Clear the entire list
 const clearList = () => {
     let liItems = document.querySelectorAll('.listItem');
+    taskArray = loadFromStorage();
     liItems.forEach((elem) => {
         elem.remove();
-        localStorage.clear()
     })
+    localStorage.clear();
+    taskArray = [];
 }
 
-
-
 // Listeners
-document.querySelector("#addBtn").addEventListener("click",itemAdd); // Addition
-document.querySelector("#addBtn").addEventListener("click",saveToStorage);
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const label = inputField.value;
+    const id = incrementId()
+
+    const tasks = loadFromStorage();
+    tasks.push({id, label});
+    saveToStorage(tasks);
+    form.reset()
+})
+    
+// Event listener for adding items using enter key
+    
+form.addEventListener('keydown', e => {
+if (e.key === "Enter") {
+    e.preventDefault(); // Disable key default functionality
+    createTask();
+    form.reset();
+    }
+    
+});
+//document.querySelector("#addBtn").addEventListener("click",saveToStorage); // Addition
+//document.querySelector("#addBtn").addEventListener("click",saveToStorage);
 document.querySelector("#delBtn").addEventListener("click",delSelected); // Deletion
 document.querySelector("#clearBtn").addEventListener("click",clearList);
 
-// Event listener for adding items using enter key
-inputField.addEventListener('keypress',function(e) {
-    if (e.key === "Enter") {
-        e.preventDefault(); // Disable key default functionality
-        document.querySelector('#addBtn').click();
+// Load storage on page load
+window.onload = function () {
+    const taskData = loadFromStorage();
+    /*
+    for (let i = 0; i < taskData.length; i++) {
+        insertToTasks(taskData[i].label)
     }
-});
+    */
+}
